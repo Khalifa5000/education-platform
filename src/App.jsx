0b1278f877292b2d68643c1eda54aa551a8a9fb6
khalifa5000 <m.khalifa5000@gmail.com>
2026-06-20 00:16:45 +0300

    Edit Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Nav from './components/nav/Nav.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import './App.css'
 
@@ -7,9 +10,15 @@ function App() {
 
   return (
     <>
+      <Nav />
       <div className="counter">
         <h1>Educational Platform</h1>
+        
       </div>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sed quod amet cupiditate distinctio repellat ea adipisci, magni officiis, saepe, earum atque eius ratione architecto pariatur. Beatae maxime dolores esse!
+        Laboriosam non neque aperiam eligendi, aspernatur deleniti obcaecati reprehenderit odio officiis tempora nobis temporibus earum aut cum minima perferendis fugiat facilis alias sit! Pariatur corporis minus deserunt vero quae! Consequatur!
+        Cum quae illo necessitatibus magnam tempore recusandae blanditiis, debitis sit corporis sed neque ducimus corrupti maxime quaerat accusamus alias sint tenetur vitae labore eos libero! Voluptates soluta culpa expedita.
+        </span>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../nav.css';
+const Nav = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Courses</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Nav;
