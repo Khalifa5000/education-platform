33b7506a4760c22624f1752ea45757d9259277eb
khalifa5000 <m.khalifa5000@gmail.com>
2026-06-20 00:17:16 +0300

    Edit Nav

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import Nav from './components/nav/Nav.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
